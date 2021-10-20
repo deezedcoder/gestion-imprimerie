14a8c2ced81473a2770989e8bb59bf59dc756231
Madjid TOLBA <deezedcoder@gmail.com>
2021-10-20 09:01:22 +0100

    import data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ImportButton from './components/ImportButton';
 function App() {
   return (
     <div className="App">
-      <ImportButton src="pdf_file" />
+      <ImportButton src="data/bsm.pdf" />
     </div>
   );
 }
